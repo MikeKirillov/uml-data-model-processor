aa8c60fab4c1d20de45f7264d88d89116abd5ec8
Mikhail Kirillov <kirilov.mikhail91@yandex.ru>
2025-03-18 00:11:20 +0300

    to squash later. processing lines to entities map

--- src/test/java/ParserTest.java
+++ src/test/java/ParserTest.java
@@ -1,16 +1,26 @@
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ParserTest {
     private static final String RESOURCES_PATH = "src/test/resources/";
     private static final String TXT_FILE_PATH = "data-base-model.txt";
     private static final String PU_FILE_PATH = "data-base-model.pu";
     private static final String PUML_FILE_PATH = "data-base-model.puml";
+
+    private static final String TAG_START = "@startuml";
+    private static final String TAG_END = "@enduml";
+    private static final String TAG_TYPE_ENTITY = "entity";
+    private static final String TAG_TYPE_CLASS = "class";
+    private static final String TAG_AS = " as ";
 
     @Test
     public void shouldReadLines() throws IOException {
@@ -21,6 +31,94 @@ public class ParserTest {
                 .map(String::trim)
                 .collect(Collectors.toList());
 
-        cleanSpaces.forEach(System.out::println);
+        assertEquals(TAG_START, cleanSpaces.get(0));
+        assertEquals(TAG_END, cleanSpaces.get(cleanSpaces.size() - 1));
+
+        // 1. Extracting every entity lines as map
+        Map<String, List<String>> entitiesMap = new HashMap<>();
+        Iterator<String> iterator = cleanSpaces.iterator();
+        String lastKey = null;
+        List<String> entityInnerLines = new ArrayList<>();
+
+        while (iterator.hasNext()) {
+            String line = iterator.next();
+
+            if (line.contains(TAG_TYPE_ENTITY) || line.contains(TAG_TYPE_CLASS)) {
+                int first = line.indexOf("\"") + 1;
+                int second = line.lastIndexOf("\"");
+                lastKey = line.substring(first, second);
+            }
+
+            if (lastKey != null) {
+                if (line.equals("}")) {
+                    entityInnerLines.add(line);
+                    entitiesMap.put(lastKey, entityInnerLines);
+                    lastKey = null;
+                    entityInnerLines = new ArrayList<>();
+                } else {
+                    entityInnerLines.add(line);
+                }
+            }
+        }
+
+        entitiesMap.forEach((k, v) -> System.out.println(k + ": " + v));
+
+        // 2. Process each entity value from map
+        List<Entity> entities = new ArrayList<>();
+        /*
+        for (String line : cleanSpaces) {
+            String entityName = null;
+            String entityAlias = null;
+
+            // get entity name between quotes ("..."). no single quotes used at entity naming
+            if (line.contains(TAG_TYPE_ENTITY) || line.contains(TAG_TYPE_CLASS)) {
+                int first = line.indexOf("\"") + 1;
+                int second = line.lastIndexOf("\"");
+                entityName = line.substring(first, second);
+            }
+
+            // check if entity has alias
+            if (line.contains(TAG_AS)) {
+                int first = line.lastIndexOf(TAG_AS) + TAG_AS.length();
+                String alias = line.substring(first);
+                *//*TODO:
+         * check is missing: alias and its mentions at relations are not equals
+         * (see PUML situations with creating empty entity)
+         *//*
+                entityAlias = alias.substring(0, alias.indexOf("{")).trim();
+            }
+
+            if (Objects.nonNull(entityName)) {
+                entities.add(new Entity(entityName, entityAlias)); // TODO: OK
+            }
+        }
+
+        assertEquals(3, entities.size());
+
+
+        //=========////=========////=========////=========////=========//
+        entities.forEach(System.out::println);
+        cleanSpaces.forEach(System.out::println); // TODO DELETE
+        */
+    }
+
+    static class Entity {
+        String name;
+        String alias;
+
+        Entity(String name, String alias) {
+            Objects.requireNonNull(name, "name");
+
+            this.name = name;
+            this.alias = alias;
+        }
+
+        @Override
+        public String toString() {
+            return "Entity{" +
+                    "name=" + name +
+                    ", alias=" + alias +
+                    '}';
+        }
     }
 }
